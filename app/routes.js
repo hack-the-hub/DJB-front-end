17b950c3a7f9013c7c098c9145a6f15a145fafa7
Rory Hanratty <rory@pengin.pro>
2016-09-10 13:23:08 +0100

    Add route and template for listing organisations
    Adding the route and template for listing organisations, expecting a json array of
    organisation objects. Maybe need to rename to volunteer orgs or something. Anyway, this is
    in place for us to iterate on now.

--- app/routes.js
+++ app/routes.js
@@ -34,6 +34,43 @@ router.get('/orgs/view/:charity_id', function(req, res) {
 
 })
 
+router.get('/orgs/view/', function(req, res) {  
+    // This is where we'd make a request to an API
+    // request('url for aPI goes here' + req.params.charity_id, function(error, response, body) {
+    //     if (!error && response.statusCode == 200) {
+    //         // var postcode = JSON.parse(body).result;
+    //         console.log(postcode);
+    //         res.render('orgs/view', { 'charity': charity })
+    //     }
+    //     else{
+    //     res.render('ohsnap/404', { 'charity': charity })
+    //     }
+    // });
+
+    var charities = [{
+        "charity_id": "12345",
+        "name": "Awesome Badger Charity",
+        "registered": "19790201",
+        "address": "1, the street, the town, the world, BT1XXp",
+        "website": "http://example.org",
+        "email": "blah@example.org",
+        "phone": "28949128741",
+        "mission": "to save all the badgers from guns"
+    },{
+        "charity_id": "54321",
+        "name": "Coding for a Purpose",
+        "registered": "19790201",
+        "address": "1, the street, the town, the world, BT1XXp",
+        "website": "http://example.org",
+        "email": "blah@example.org",
+        "phone": "28949128741",
+        "mission": "Writing code to make the world a better place. For Free."
+    }];
+
+    res.render('orgs/list', { 'charities': charities })
+
+})
+
 // add your routes here
 
 module.exports = router
